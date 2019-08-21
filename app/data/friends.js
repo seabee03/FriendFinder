331c34ab4243a394797314bccb206394c1e113f3
DJ Prado <darcus.prado@gmail.com>
2019-08-21 14:21:37 -0500

    added code to server.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,17 @@
 var friends = [
     {
+        "name": "Mr. Krabs"
+        "photo": "./assets/images/MrKrabs.svg"
+        "scores": ["4", "2", "1", "5", "3", "2", "1", "5", "3", "3"]
+    },
+    {
         "name": "SpongeBob SquarePants"
-        "photo":
-    }
+        "photo": "./assets/images/SpongeBobSquarePants.svg"
+        "scores": ["2", "2", "3", "3", "5", "1", "1", "4", "3", "5"]
+    },
+    {
+        "name": "Patrick Star"
+        "photo": "./assets/images/PatrickStar.svg"
+        "scores": ["2", "4", "3", "4", "5", "5", "2", "2", "3", "1"]
+    },
 ]
